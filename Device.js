fb17abaf801bab58fa8314bbe196ddccb823c206
JagathVM <17jagathvm@gmail.com>
2026-01-12 13:54:52 +0530

    Add Fan device implementing start and stop behavior

--- Device.js
+++ Device.js
@@ -26,3 +26,28 @@ class Device {
     throw new Error("stop() must be implemented by subclasses!");
   }
 }
+
+// ----- Fan Device -----
+class Fan extends Device {
+  // Method to start the device
+  start() {
+    // Check whether the device is ON
+    if (!this.isOn()) {
+      // Turn ON the device
+      this._turnOn();
+      // Print the output
+      console.log("Fan has started!");
+    }
+  }
+
+  // Method to stop the device
+  stop() {
+    // Check whether the device is ON
+    if (this.isOn()) {
+      // Turn OFF the device
+      this._turnOff();
+      // Print the output
+      console.log("Fan has stopped!");
+    }
+  }
+}
