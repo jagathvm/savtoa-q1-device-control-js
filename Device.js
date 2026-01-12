c07b448eb6d94b0d37cee942334a1fd47dbc2de8
JagathVM <17jagathvm@gmail.com>
2026-01-12 13:36:38 +0530

    Add protected power control methods to Device class

--- Device.js
+++ Device.js
@@ -2,4 +2,13 @@
 class Device {
   // Private inner state
   #isOn = false;
+
+  // Protected helper methods
+  _turnOn() {
+    this.#isOn = true;
+  }
+
+  _turnOff() {
+    this.#isOn = false;
+  }
 }
