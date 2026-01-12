f17b18602c0db2528f72dbc628c626e2db7ac8cb
JagathVM <17jagathvm@gmail.com>
2026-01-12 14:33:26 +0530

    Demonstrate abstract Device usage error

--- Device.js
+++ Device.js
@@ -64,14 +64,23 @@ class Controller {
 }
 
 // Create instance of Fan device
-const fan = new Fan();
+// const fan = new Fan();
 
 // Create instance of Controller
-const controller = new Controller();
+// const controller = new Controller();
 
 // Controller calls the operate method on device
-controller.operate(fan);
+// controller.operate(fan);
 
 // ----- Output -----
 // Fan has started!
 // Fan has stopped!
+
+// Create instance of Device
+const device = new Device();
+
+// Calling start method directly on the device
+device.start();
+
+// ----- Output -----
+// Error: start() must be implemented by subclasses!
